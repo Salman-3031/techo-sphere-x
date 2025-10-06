512ca17a5b98dea2ab9a7ca2a77a8e5340b3d1f9
Salman-3031 <salman5223031@gmail.com>
2025-10-06 15:34:58 +0500

    contact changes

--- src/components/Contact/FindUsOnGoogle.jsx
+++ src/components/Contact/FindUsOnGoogle.jsx
@@ -8,7 +8,7 @@ const FindUsOnGoogle = () => {
       <div className="container text-center bg-bglight dark:bg-bgdark">
        
         <a
-          href="https://www.google.com/maps/@34.7964373,72.4140418,18z?entry=ttu&g_ep=EgoyMDI1MDkyOS4wIKXMDSoASAFQAw%3D%3D"
+          href="https://maps.app.goo.gl/xuat3LdWgZynQuy37"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-block bg-primary text-white font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-primary/90 transition duration-300"
@@ -19,14 +19,11 @@ const FindUsOnGoogle = () => {
         {/* Optional: Embed Google Map preview */}
         <div className="mt-10">
           <iframe
-            title="Google Map"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1721.2015010759474!2d72.41416233273027!3d34.796375951121256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1759380564753!5m2!1sen!2s"
-            width="100%"
-            height="400"
-            allowFullScreen=""
-            loading="lazy"
-            className="rounded-lg shadow-lg"
-          ></iframe>
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d623.5021757855934!2d72.44127016296456!3d34.85155886356759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38dc3b0072656433%3A0xd48ab3f2214d4853!2sTechnoSphereX%20Software%20House!5e1!3m2!1sen!2s!4v1759746697623!5m2!1sen!2s" 
+          width="100%" height="450" 
+          allowfullscreen="" 
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </section>
