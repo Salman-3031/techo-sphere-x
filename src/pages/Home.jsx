2f7811c1cfd71bac31b9a3f67eba9c51f8239610
Salman-3031 <salman5223031@gmail.com>
2025-10-06 16:00:29 +0500

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HomeHero from '../components/HomeComponent/homeHero'
+import HomeHero from '../components/HomeComponent/HomeHero'
 import HomeServices from '../components/HomeComponent/HomeServices'
 import HomeCollaboration from '../components/HomeComponent/HomeCollaboration'
 
